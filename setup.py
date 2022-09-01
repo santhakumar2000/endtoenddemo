913500bcd387d57b8e3e849336be75a555af8495
santhakumar2000 <santhakumardadala2000@gmail.com>
2022-09-01 11:48:18 +0530

    Adding cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='end to end machine learning pipeline with mlops tools',
+    author='santhakumar',
+    license='MIT',
+)
